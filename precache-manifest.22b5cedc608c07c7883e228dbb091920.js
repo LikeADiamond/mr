00892985d5d7e941f53d4215d96006120f2c60cb
LikeADiamond <97adss97@gmail.com>
2020-03-07 17:04:39 +0100

    Updates

---
+++ precache-manifest.22b5cedc608c07c7883e228dbb091920.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e781a11e654b10c40349a2bae197cc",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b6ef4dc69cd6224178e6",
+    "url": "/monster-rolodex/static/css/main.8086e6e2.chunk.css"
+  },
+  {
+    "revision": "6b83838d96d229dfb591",
+    "url": "/monster-rolodex/static/js/2.64be321a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-rolodex/static/js/2.64be321a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6ef4dc69cd6224178e6",
+    "url": "/monster-rolodex/static/js/main.aaf12b7e.chunk.js"
+  },
+  {
+    "revision": "85cb2c00df0e1ad7d48d",
+    "url": "/monster-rolodex/static/js/runtime-main.3e508d1c.js"
+  }
+]);
